c89db627c7cb69174f329566c4849910659fef30
SFS FUND <ebhoriajosh@gmail.com>
2022-10-15 09:51:42 +0100

    completed setup with webpack

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SFS from './asset/sfs.png'
+import './global/styles.css'
+function App() {
+  // const bo = 'namespace'
+  return (
+    <div>
+      <div>
+        <h1>Welcome to Taxaide Assessments</h1>
+      </div>
+      <img src={SFS} alt="placeholder" />
+    </div>
+  )
+}
+
+export default App
